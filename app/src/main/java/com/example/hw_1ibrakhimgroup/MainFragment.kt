75009797b13dfa1fc52d003c7b985de504ebe997
Timur2010-creator <timuraitbaev7@gmail.com>
2025-11-03 20:32:41 +0600

    changed the code in activitymain in mainactivity and in mainfragment

--- app/src/main/java/com/example/hw_1ibrakhimgroup/MainFragment.kt
+++ app/src/main/java/com/example/hw_1ibrakhimgroup/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hw_1ibrakhimgroup.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -24,8 +25,9 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loadData()
-        binding.rvSerial.adapter
         adapter = SerialAdapter(listSerial)
+        binding.rvSerial.adapter = adapter
+        binding.rvSerial.layoutManager = LinearLayoutManager(requireContext())
     }
 
     fun loadData() {
